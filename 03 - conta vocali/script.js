d7a9e9563208374cfa3d3a810d68cb8939b491f7
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-06 10:13:54 +0100

    exc 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const countVowels = (word) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let counterVowels = 0
 
+    for(let i = 0; i < word.length; i++) {
+        const currentLetter = word[i];
+
+        if(vowels.includes(currentLetter)) {
+            counterVowels++;
+        }
+    }
+    return counterVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result =countVowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
